19492e5d4a4021ffe0335f8f376fe594fc894d89
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-09-12 10:47:59 -0700

    Add initial files.

---
+++ thoth/__init__.py
@@ -0,0 +1,14 @@
+"""
+A declarative way of invoking sphinx, 
+
+And writing the configuration in pyproject.toml
+
+This module is more opinionated than sphinx.
+"""
+
+__version__ = '0.0.1'
+
+
+
+def main():
+    print('it works')
