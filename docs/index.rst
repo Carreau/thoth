5bf9bb485cc807204a726711e5150d42231d26a8
Matthias Bussonnier <bussonniermatthias@gmail.com>
2016-09-12 22:15:58 -0700

    init travis

---
+++ docs/index.rst
@@ -0,0 +1,14 @@
+Thoth
+=====
+
+An opinionated documentation builder based on sphinx.
+
+Thoth is meant to be zero-configuration, it should works out of the box, and is
+meant to publish multiple version of a documentation easy. 
+
+Create an ``index.rst`` in a folder and run ``thoth``, your documentation will
+be automatically built, in ``_build/html/X.y.z``. Thoth does not handle
+auto-detecting versions numbers yet.
+
+
+
--- thoth/__init__.py
+++ thoth/__init__.py
@@ -8,7 +8,25 @@ This module is more opinionated than sphinx.
 
 __version__ = '0.0.1'
 
+from sphinx import cmdline
+from sphinx.application import Sphinx
+
+import os
+import os.path
+
+
 
 
 def main():
+    print('sphinx-build -b html -d _build/doctrees . _build/html')
+    cmdline.main('sphinx-build -b html -d _build/doctrees . _build/html'.split(' '))
+    here = os.getcwd()
+    srcdir = here
+    confdir = os.path.join(here,'docs')
+    confdir = None
+    outdir = os.path.join(here,'_build/html/{version}'.format(version='X.y.z'))
+    doctreedir = os.path.join(here,'.cache')
+
+    builder = Sphinx(srcdir, confdir, outdir, doctreedir, 'html', confoverrides={'master_doc':'index'})
+    builder.build(None, [])
     print('it works')
